35291435ba073ad8eb0e2467fa205c54e074ceb1
KENTO OSAWA <xdays1002@gmail.com>
2022-07-25 15:38:30 +0900

    port

--- main.py
+++ main.py
@@ -127,6 +127,6 @@ def upload_file():
 #     app.run("0.0.0.0", debug=True)
     
     
-if __name__ == “__main__“:
-    port = int(os.environ.get(‘PORT’, 8080))
-    app.run(host =‘0.0.0.0’,port = port)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ="0.0.0.0",port = port)
