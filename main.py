41855653e890f59fbd241833267536147ac8857d
KENTO OSAWA <xdays1002@gmail.com>
2022-07-27 12:46:53 +0900

    try

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def upload_file():
             return redirect(url_for('download_api'))
         
         
-    return render_template("index.html",answer="")
+    return render_template("about.html",answer="")
 
 
 
@@ -124,7 +124,13 @@ def upload_file():
 
 
 
+#これが正規。    
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 8080))
+#     app.run(host ="0.0.0.0",port = port)
     
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
-    app.run(host ="0.0.0.0",port = port)
+    app.run("0.0.0.0", debug=True)
+    
